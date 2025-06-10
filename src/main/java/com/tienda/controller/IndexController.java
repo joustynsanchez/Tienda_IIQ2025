041d89b1436d6107b9fcdb91d82bfca2d7fcc700
joustynsanchez <jostinsanchez07@gmail.com>
2025-06-09 22:49:46 -0600

    Semana 4

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+    @RequestMapping("/informacion")
+    public String info() {
+        return "info";
+    }
+   
+}
